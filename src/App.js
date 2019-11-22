45c90f019a74d8731fdaf72dc3f7949f98a37b55
spencerlindemuth <lindemut@gmail.com>
2019-11-22 13:24:47 -0800

    Wrote ref controlled input

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component{
 
-export default App;
+  constructor(props){
+    super(props)
+    this.textInput = React.createRef()
+    this.state = {
+      input: ""
+    }
+  }
+
+  handleChange = () => {
+    this.setState({
+      input: this.textInput.current.value
+    })
+  }
+
+  clearInput = () => {
+    this.setState({
+      input: ""
+    })
+    this.textInput.current.focus()
+  }
+
+  render(){
+      return (
+        <div className="App">
+          <label htmlFor="name-input">Please input your name</label>
+          <br />
+          <input ref={this.textInput} value={this.state.input} onChange={this.handleChange}/>
+          <br />
+          <button onClick={() => this.textInput.current.focus()}>Set Input Field Focus</button>
+          <button onClick={this.clearInput}>Clear Input Field</button>
+        </div>
+      );
+    }
+}
